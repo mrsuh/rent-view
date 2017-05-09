75f234fed2f20f5223998b8d43266746ffcdb165
anton <mrsuh6@gmail.com>
2017-05-09 14:29:44 +0300

    repo hot/cold

--- gruntfile.js
+++ gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
                         },
                         {
                             pattern: '{{version}}',
-                            replacement: 'v0.5.3 beta'
+                            replacement: 'v0.5.4 beta'
                         }
                     ]
                 }
