603738691d15d5d4c01fda636cd33fa87b8b8acf
anton <mrsuh6@gmail.com>
2017-05-28 20:18:34 +0300

    remove write icon

--- gruntfile.js
+++ gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
                         },
                         {
                             pattern: '{{version}}',
-                            replacement: 'v1.0.1'
+                            replacement: 'v1.0.2'
                         }
                     ]
                 }
