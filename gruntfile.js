0fa30caedfd152867fdac70e3c3dc123714ff38d
anton <mrsuh6@gmail.com>
2017-11-22 23:55:47 +0300

    version

--- gruntfile.js
+++ gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
                         },
                         {
                             pattern: '{{version}}',
-                            replacement: 'v3.0.0'
+                            replacement: 'v3.0.1'
                         }
                     ]
                 }
